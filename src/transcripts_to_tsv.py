be6a935bb61b397468802c76a72a10f1c435d874
Daniel John Benton <danieljbwork@gmail.com>
2018-03-14 20:56:29 +0000

    titles output

--- src/transcripts_to_tsv.py
+++ src/transcripts_to_tsv.py
@@ -1,14 +1,26 @@
-from os import listdir
+from os import listdir, makedirs
+from operator import itemgetter
 from scrape_transcript import scrape_transcript
 
-seasons = (1, 2, 3, 4, 5, 6)
+seasons = (1, 2, 3, 4, 6)
+
+episodesOutput = ""
 
 for season in seasons:
 	directory = f"downloaded_pages/season_{season}"
 	fileNames = [fileName for fileName in listdir(directory) if fileName.endswith(".html")]
+	
+	# sort numerically again because I forgot to left-pad the numbers so the episodes get sorted out of order by listdir:
+	fileNamesPieces = []
 	for fileName in fileNames:
-		#if "beast" not in fileName.lower():
-			#continue
+		pieces = fileName.split("_")
+		part1 = int(pieces[0])
+		part2 = fileName
+		fileNamesPieces.append([part1, part2])
+	fileNamesPieces.sort(key=itemgetter(0)) # https://stackoverflow.com/a/4174955
+	fileNames = [pieces[1] for pieces in fileNamesPieces]
+	
+	for fileName in fileNames:
 		filePath = f"{directory}/{fileName}"
 		print(filePath)
 		
@@ -16,3 +28,7 @@ for season in seasons:
 			transcriptHtml = transcriptFile.read()
 		
 		title, lines = scrape_transcript(transcriptHtml, filePath)
+		
+		episodesOutput += f"{title}\t{season}\t{filePath}\n"
+
+print(episodesOutput)
