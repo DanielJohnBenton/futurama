68e2355ddb87cb36732c0a40f927eada4e6b99e1
Daniel John Benton <danieljbwork@gmail.com>
2018-03-17 16:46:46 +0000

    fix tokenisation for single quotes

--- src/tokenise.py
+++ src/tokenise.py
@@ -1,31 +1,44 @@
 def tokenise(text):
-    onSpace = True
-    processedText = ""
-    for character in text:
-        if character.isalnum():
-            processedText +=""
-            onSpace = False
-    
-    xLastCharacter = len(text) - 1
-    for iCharacter in range(len(text)):
-        character = text[iCharacter]
-        if character == "-":
-            if processedText != "" and text[iCharacter - 1].isalnum() and iCharacter != xLastCharacter and text[iCharacter + 1].isalnum():
-                processedText += character
-            elif not onSpace:
-                processedText +=" "
-                onSpace = True
-        elif character == ".":
-            if processedText != "" and text[iCharacter - 1].isdigit() and iCharacter != xLastCharacter and text[iCharacter + 1].isdigit():
-                processedText += character
-            elif not onSpace:
-                processedText +=" "
-                onSpace = True
-        elif character.isalnum():
-            processedText += character
-            onSpace = False
-        elif not onSpace:
-            processedText +=" "
-            onSpace = True
-    
-    return processedText.strip().split(" ")
+	onSpace = True
+	processedText = ""
+	for character in text:
+		if character.isalnum():
+			processedText +=""
+			onSpace = False
+	
+	xLastCharacter = len(text) - 1
+	for iCharacter in range(len(text)):
+		character = text[iCharacter]
+		
+		if character == "-":
+			if processedText != "" and text[iCharacter - 1].isalnum() and iCharacter != xLastCharacter and text[iCharacter + 1].isalnum():
+				processedText += character
+				onSpace = False
+			elif not onSpace:
+				processedText +=" "
+				onSpace = True
+		
+		elif character == ".":
+			if processedText != "" and text[iCharacter - 1].isdigit() and iCharacter != xLastCharacter and text[iCharacter + 1].isdigit():
+				processedText += character
+			elif not onSpace:
+				processedText +=" "
+				onSpace = True
+		
+		elif character == "'":
+			if processedText != "" and text[iCharacter - 1].isalpha() and iCharacter != xLastCharacter and text[iCharacter + 1].isalpha():
+				processedText += character
+				onSpace = False
+			elif not onSpace:
+				processedText +=" "
+				onSpace = True
+		
+		elif character.isalnum():
+			processedText += character
+			onSpace = False
+		
+		elif not onSpace:
+			processedText +=" "
+			onSpace = True
+	
+	return processedText.strip().split(" ")
